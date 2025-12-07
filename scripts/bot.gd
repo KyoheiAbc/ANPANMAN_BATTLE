c816d6ab5ac313fc89cdb742c47462ee17fe48b3
KyoheiAbc <autumn8november@gmail.com>
2025-12-07 22:03:14 +0900

    Adjust character HP maximum and refine attack logic for improved gameplay dynamics

--- scripts/bot.gd
+++ scripts/bot.gd
@@ -8,7 +8,7 @@ var rival: Character
 var frame_count: int = 0
 var walk_direction: int = 0
 
-var attack_probability_close = 0.2
+var attack_probability_close = 1.0
 
 
 func _init(character: Character, rival: Character) -> void:
@@ -18,13 +18,17 @@ func _init(character: Character, rival: Character) -> void:
 	frame_count = randi_range(16, 32)
 	walk_direction = character.direction
 
+	character.attack_cool_time_max /= 3.0
+	character.special_cool_time_max /= 2.0
+
 
 func process() -> void:
 	if character.state == Character.State.LOSE:
 		return
 
 	if character.state == Character.State.ATTACKING:
-		character.attack()
+		if randf() < 1 / 15.0:
+			character.attack()
 
 	frame_count -= 1
 	if frame_count < 0:
@@ -34,18 +38,19 @@ func process() -> void:
 			walk_direction = 0 if randf() < 0.666 else character.direction * -1
 
 
-	if randf() < 1 / 60.0:
+	if randf() < 1 / 180.0:
 		character.jump()
 
 	if randf() < 1 / 60.0:
 		character.special()
 
-	if randf() < 1 / 180.0:
+	if randf() < 1 / 300.0:
 		character.attack()
 
+
 	var distance = abs(character.position.x - rival.position.x)
-	if distance < 250:
+	if distance < 200:
 		if randf() < attack_probability_close:
-			character.attack()
+		character.attack()
 
 	character.walk(walk_direction)
